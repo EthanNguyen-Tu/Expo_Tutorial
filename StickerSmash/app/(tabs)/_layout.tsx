892900a0796625e1f2cd7bbb27297f3d4778ba12
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-05-27 18:21:16 -0400

    feat(Ch2): Tab bar icons

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
     return (
@@ -15,8 +16,36 @@ export default function TabLayout() {
                 },
             }}
         >
-            <Tabs.Screen name="index" options={{ title: "Home" }} />
-            <Tabs.Screen name="about" options={{ title: "About" }} />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons
+                            name={focused ? "home-sharp" : "home-outline"}
+                            color={color}
+                            size={24}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="about"
+                options={{
+                    title: "About",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons
+                            name={
+                                focused
+                                    ? "information-circle"
+                                    : "information-circle-outline"
+                            }
+                            color={color}
+                            size={24}
+                        />
+                    ),
+                }}
+            />
         </Tabs>
     );
 }

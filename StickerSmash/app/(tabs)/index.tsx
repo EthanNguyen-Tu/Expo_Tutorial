1b257797636b552e848d76a9be02368059c017fc
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-05-29 20:17:01 -0400

    feat(Ch5): Basic Button

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -6,6 +6,53 @@ import { useState } from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
+export default function Index() {
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(
+        undefined
+    );
+    const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images"],
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setShowAppOptions(true);
+        } else {
+            alert("You did not select any iamge.");
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer
+                    imgSource={PlaceholderImage}
+                    selectedImage={selectedImage}
+                />
+            </View>
+            {showAppOptions ? (
+                <View />
+            ) : (
+                <View style={styles.footerContainer}>
+                    <Button
+                        theme="primary"
+                        label="Choose a photo"
+                        onPress={pickImageAsync}
+                    />
+                    <Button
+                        label="Use this photo"
+                        onPress={() => setShowAppOptions(true)}
+                    />
+                </View>
+            )}
+        </View>
+    );
+}
+
 export const styles = StyleSheet.create({
     container: {
         backgroundColor: "#25292e",
@@ -29,42 +76,4 @@ export const styles = StyleSheet.create({
         alignItems: "center",
     },
 });
-
-export default function Index() {
-    const [selectedImage, setSelectedImage] = useState<string | undefined>(
-        undefined
-    );
-    const pickImageAsync = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ["images"],
-            allowsEditing: true,
-            quality: 1,
-        });
-
-        if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri);
-        } else {
-            alert("You did not select any iamge.");
-        }
-    };
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer
-                    imgSource={PlaceholderImage}
-                    selectedImage={selectedImage}
-                />
-            </View>
-            <View style={styles.footerContainer}>
-                <Button
-                    label="Choose a photo"
-                    theme="primary"
-                    onPress={pickImageAsync}
-                />
-                <Button label="Use this photo" />
-            </View>
-        </View>
-    );
-}
 
